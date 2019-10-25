27f346ae070d8b0543cb426706e7bf46436d99f6
nessa-waithaka <vanessa.waithaka@gmail.com>
2019-10-25 15:01:20 +0300

    Add getElementById function on js

---
+++ js/script.js
@@ -0,0 +1,6 @@
+function myFunc() {
+    let d = document.getElementById("day").value;
+    let m = document.getElementById("month").value;
+    let y = document.getElementById("year").value;
+    let g = document.getElementById("gender").value;
+}
