4cfa0e5afd7f41f6a01453e4074a7d6ff9d37777
nessa-waithaka <vanessa.waithaka@gmail.com>
2019-10-25 15:03:09 +0300

    Implement day of the week formula

--- js/script.js
+++ js/script.js
@@ -3,4 +3,14 @@ function myFunc() {
     let m = document.getElementById("month").value;
     let y = document.getElementById("year").value;
     let g = document.getElementById("gender").value;
+
+    let CC = parseInt(y.slice(0, 2));
+    let YY = parseInt(y.slice(2, 4));
+    let MM = parseInt(m);
+    let DD = parseInt(d);
+
+    let birthday = (((CC / 4) - 2 * CC - 1) + ((5 * YY / 4)) + ((26 * (MM + 1) / 10)) + DD) % 7;
+    let birthdate = birthday.toFixed(0);
+
+    //document.getElementById("result").innerHTML = birthdate;
 }
